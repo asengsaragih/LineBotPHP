ed5abcb28838e8fb21476514fb50ff08e3b7a95f
aldi saragih <aldiwahyu.saragih@gmail.com>
2019-07-01 18:41:15 +0700

    implementasi flexbox

--- index.php
+++ index.php
@@ -88,7 +88,32 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                         return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
                     }
                 } else {
-                //message from single user
+                    if ($event['message']['type'] == 'text') {
+                        if (strtolower($event['message']['text']) == 'user id') {
+ 
+                            $result = $bot->replyText($event['replyToken'], $event['source']['userId']);
+ 
+                        } elseif (strtolower($event['message']['text']) == 'flex message') {
+ 
+                            $flexTemplate = file_get_contents("flex_message.json"); // template flex message
+                            $result = $httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
+                                'replyToken' => $event['replyToken'],
+                                'messages'   => [
+                                    [
+                                        'type'     => 'flex',
+                                        'altText'  => 'Test Flex Message',
+                                        'contents' => json_decode($flexTemplate)
+                                    ]
+                                ],
+                            ]);
+ 
+                        } else {
+                            // send same message as reply to user
+                            $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+                        }
+ 
+                        return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
+                    }
                 }
             }
         } 
