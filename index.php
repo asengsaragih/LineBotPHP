591987c1e489392272b81c2aad71302894cec3c3
aldi saragih <aldiwahyu.saragih@gmail.com>
2019-07-04 01:56:22 +0700

    inisial line bot master 6

--- index.php
+++ index.php
@@ -164,7 +164,7 @@ $app->get('/pushmessage', function($req, $res) use ($bot)
         $textMessageBuilder = new TextMessageBuilder($isiPesan);
         $result = $bot->pushMessage($userId, $textMessageBuilder);
         return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
-        header('Location: '.$_SERVER['REQUEST_URI']);
+        header('Location: index.php/pushmessage');
     }
     
     //yang dibawah untuk push stiker
