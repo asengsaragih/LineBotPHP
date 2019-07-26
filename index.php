7e5b1bbffec4ab22ac40b43adf36e6cc1f3219fd
aldi saragih <aldiwahyu.saragih@gmail.com>
2019-07-26 22:02:47 +0700

    tes flex

--- index.php
+++ index.php
@@ -74,6 +74,19 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                         ],
                     ]);
                     break;
+                case '/flex':
+                    $flexTemplate = file_get_contents("jadwal.json"); // template flex message
+                    $result = $httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
+                        'replyToken' => $event['replyToken'],
+                        'messages'   => [
+                                    [
+                                'type'     => 'flex',
+                                'altText'  => 'Test Flex Message',
+                                'contents' => json_decode($flexTemplate)
+                            ]
+                        ],
+                    ]);
+                    break;
                 case '/kuliah':
                     $nama_hari = date("l");
                     $jadwal_Matkul = "";
