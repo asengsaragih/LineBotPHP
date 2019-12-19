2f4b2dd676151569ae867921f843b4ee34eebac8
aldi saragih <aldiwahyu.saragih@gmail.com>
2019-12-20 00:04:33 +0700

    initial commit

--- index.php
+++ index.php
@@ -59,7 +59,9 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
         foreach ($data['events'] as $event) {
             date_default_timezone_set("Asia/Jakarta");
             $userMessage = strtolower($event['message']['text']);
-            $idGrupKelas = "C0964f2cf09b447618a304da9c2219993";
+            $idGrupKelas = "Ca01deda556b50eb3941eb720de9a60fe";
+            $idGrupPrivate = "C0964f2cf09b447618a304da9c2219993";
+            $id_aseng = "Udb1551f3893cade017d14653f2b186c3";
             $cekGrup = $event['source']['groupId'];
             $idRoom = $event['source']['roomId'];
             $idGroup = $event['source']['groupId'];
@@ -67,6 +69,8 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
 
             $errorTextInfo = "Hanya Bisa Dilakukan Digrup Kelas D3IF41-03";
             $errorText = "Tidak Bisa Menjalankan Perintah Ini";
+            $datanullTextInfo = "Data Kosong";
+            $manyDataTextInfo = "Data Terlalu Banyak";
 
             $userId     = $event['source']['userId'];
             $getprofile = $bot->getProfile($userId);
@@ -80,6 +84,71 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
             $kamis = "Vvpl\nKU3.07.15\n06.30 - 11.30\n\nPBS\nA6\n12.30 - 16.30";
             $jumat = "Manajemen Proyek IT\nKU3.07.01\n09.30 - 11.30\n\nPengembangan Profesionalisme\nC2\n15.30 - 17.30";
             $seluruh_jadwal = "--Senin--\n\n".$senin."\n\n"."--Selasa--\n\n".$selasa."\n\n"."--Rabu--\n\n".$rabu."\n\n"."--Kamis--\n\n".$kamis."\n\n"."--Jum'at--\n\n".$jumat;
+
+            $conn = mysqli_connect('localhost','id9483525_asengsaragih','ganteng00','id9483525_aseng');
+            $date_now = date('Ymd');
+
+            // function searchData($search_word) {
+            //     $conn = mysqli_connect('localhost','id9483525_asengsaragih','ganteng00','id9483525_aseng');
+            //     $sql = "SELECT * FROM mahasiswa WHERE panggilan LIKE '%$search_word%' OR nim LIKE '%$search_word%' OR nama LIKE '%$search_word%'";
+            //     $execute_query = mysqli_query($conn, $sql);
+            //     $rowCount = mysqli_num_rows($execute_query);
+            // }
+
+            function filterWord($string, $filters) {
+                foreach ($filters as $banned_word) {
+                    if (stristr($string, $banned_word)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            $filters = array('/cari');
+
+            if (!filterWord($userMessage,$filters)) {
+                $search_word = substr($userMessage, 6);
+
+                $sql = "SELECT * FROM mahasiswa WHERE panggilan LIKE '%$search_word%' OR nim LIKE '%$search_word%' OR nama LIKE '%$search_word%'";
+
+                $execute_query = mysqli_query($conn, $sql);
+                $rowCount = mysqli_num_rows($execute_query);
+
+                if ($rowCount == 0) {
+                    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($datanullTextInfo);
+                    $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+                    return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+                } elseif ($rowCount >= 4) {
+                    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($manyDataTextInfo);
+                    $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+                    return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+                } elseif ($rowCount > 0 && $rowCount <= 3) {
+                    $textMahasiswa = "";
+                    while ($key = mysqli_fetch_array($execute_query)) {
+                        $nama_mahasiswa = $key['nama'];
+                        $nim = $key['nim'];
+                        $panggilan = $key['panggilan'];
+                        $telp = $key['telp'];
+                        $asal = $key['asal'];
+
+                        if ($cekGrup == $idGrupKelas || $cekGrup == $idGrupPrivate) {
+                            if ($idPengirim == $id_aseng) {
+                                $textMahasiswa .= "Nim : {$nim} \nNama : {$nama_mahasiswa} \nTelp : 0$telp \n\n";
+                            } else {
+                                $textMahasiswa .= "Nim : {$nim} \nNama : {$nama_mahasiswa} \n\n";
+                            }
+                        } else {
+                            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($errorTextInfo);
+                            $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+                            return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+                        }
+                    }
+
+                    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($textMahasiswa);
+                    $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+                    return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+                }
+            }
 
 
             switch ($userMessage) {
@@ -142,25 +211,45 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                     # code...
                     if ($cekGrup == $idGrupKelas) {
                         # code...
-                        $sumber = "https://asengsaragih.000webhostapp.com/LineBotAndroid/readInfo.php";
-                        $konten = file_get_contents($sumber);
-                        $dataInfo = json_decode($konten, true);
-                        $text_gabungan = "";
-                        foreach ($dataInfo as $d) { 
-                            $tanggal = $d['tanggal_pengumpulan'];
-                            $keterangan = $d['keterangan_tugas'];
-                            $tahun = substr($tanggal,0,4);
-                            $bulan = substr($tanggal,-4,-2);
-                            $hari = substr($tanggal,6);  
+                        // $sumber = "https://asengsaragih.000webhostapp.com/LineBotAndroid/readInfo.php";
+                        // $konten = file_get_contents($sumber);
+                        // $dataInfo = json_decode($konten, true);
+                        // $text_gabungan = "";
+                        // foreach ($dataInfo as $d) { 
+                        //     $tanggal = $d['tanggal_pengumpulan'];
+                        //     $keterangan = $d['keterangan_tugas'];
+                        //     $tahun = substr($tanggal,0,4);
+                        //     $bulan = substr($tanggal,-4,-2);
+                        //     $hari = substr($tanggal,6);  
+                        //     $gabungan_tanggal = $hari."-".$bulan."-".$tahun;
+                        //     $text_gabungan .= "{$gabungan_tanggal}"." || "."{$keterangan}"."\n\n";
+                        // }
+                        // if ($text_gabungan == null) {
+                        //     $text_gabungan = "Kosong";
+                        // }
+                        // $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text_gabungan);
+                        // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+                        // return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+                        $query = mysqli_query($conn, "select * from jadwal where tanggal_pengumpulan >= $date_now order by tanggal_pengumpulan asc");
+                        $teks_awalan = "------ INFO -----";
+                        $teks_akhir = "Untuk Penambahan Info, Silahkan Chat KM atau Aldi";
+                        $teks_info = "";
+                        while ($key = mysqli_fetch_array($query)) {
+                            $tanggal_peng = $key['tanggal_pengumpulan'];
+                            $keterangan_tgs = $key['keterangan_tugas'];
+                            $tahun = substr($tanggal_peng,0,4);
+                            $bulan = substr($tanggal_peng,-4,-2);
+                            $hari = substr($tanggal_peng,6);  
                             $gabungan_tanggal = $hari."-".$bulan."-".$tahun;
-                            $text_gabungan .= "{$gabungan_tanggal}"." || "."{$keterangan}"."\n\n";
-                        }
-                        if ($text_gabungan == null) {
-                            $text_gabungan = "Kosong";
-                        }
-                        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text_gabungan);
-                        $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-                        return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+                            $teks_info .= "{$gabungan_tanggal}"." || "."{$keterangan_tgs}"."\n";
+                            }
+                            if($teks_info == null){
+                                $teks_info = "Kosong\n";
+                            }
+                            $teks_gabungan = $teks_awalan."\n\n".$teks_info."\n".$teks_akhir;
+                            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($teks_gabungan);
+                            $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+                            return $result->getHTTPStatus() . ' ' . $result->getRawBody();
                         break;
                     } else {
                         $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($errorTextInfo);
@@ -215,27 +304,16 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                     break;
                 case '/auy':
                     # code...
-                    $auy = "Agus Suryana\n-\n-\n-";
+                    $auy = "Agus Suryana, S.S., M.Pd.\n-\nagus.nasrul.suryana@gmail.com\n0857-2110-2189";
                     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($auy);
                     $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
                     return $result->getHTTPStatus() . ' ' . $result->getRawBody();
                     break;
                 case '/ast':
                     # code...
-                    $ast = "-\n-\n-\n-";
+                    $ast = "Ahman Sutardi, M.M.-\n195808056\nahman.sutardi@gmail.com\n0813-2271-2266";
                     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($ast);
                     $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-                    return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-                    break;
-                case '/cek':
-                    $textMessageBuilder1 = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("pesan 1");
-                    $textMessageBuilder2 = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("pesan 2");
-
-                    $multiMessaegBuiler = new MultiMessageBuilder();
-                    $multiMessaegBuiler->add($textMessageBuilder1);
-                    $multiMessaegBuiler->add($textMessageBuilder2);
-
-                    $result = $bot->replyMessage($event['replyToken'], $multiMessaegBuiler);
                     return $result->getHTTPStatus() . ' ' . $result->getRawBody();
                     break;
                 case '/jadwalsenin':
@@ -269,13 +347,33 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                     return $result->getHTTPStatus() . ' ' . $result->getRawBody();
                     break;
                 //Untuk Cek Nama Pengirim Dan Cek Kode Grup 
-                case '/cek2':
+                case '/cekgrup':
                     $cek = $cekGrup." ".$namaPengirim;
                     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($cek);
                     $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
                     return $result->getHTTPStatus() . ' ' . $result->getRawBody();
                     break;
-                case '/leave':
+                // cek user id
+                case '/cekid':
+                    $cek_id = $idPengirim." ".$namaPengirim;
+                    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($cek_id);
+                    $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+                    return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+                    break;
+                // cek multi return
+                case '/cekmulti':
+                    $textMessageBuilder1 = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("pesan 1");
+                    $textMessageBuilder2 = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("pesan 2");
+
+                    $multiMessaegBuiler = new MultiMessageBuilder();
+                    $multiMessaegBuiler->add($textMessageBuilder1);
+                    $multiMessaegBuiler->add($textMessageBuilder2);
+
+                    $result = $bot->replyMessage($event['replyToken'], $multiMessaegBuiler);
+                    return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+                    break;
+                // leave group
+                case '/leavegrup':
                     if($event['source']['type'] == 'group') {
                         $response = $bot->leaveGroup($idGroup);
                         return $response->getHTTPStatus() . ' ' . $response->getRawBody();
@@ -290,7 +388,22 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                         return $result->getHTTPStatus() . ' ' . $result->getRawBody();
                         break;
                     }
-                case '/reminder':
+                case '/sholat':
+                    $linkSholat = "http://muslimsalat.com/bojongsoang/daily.json";
+                    $openContent = file_get_contents($linkSholat);
+                    $dataSholatJSON = json_decode($openContent, true);
+
+                    $subuh = "Subuh : ".$dataSholatJSON['items']['0']['fajr'];
+                    $zuhur = "Zuhur : ".$dataSholatJSON['items']['0']['dhuhr'];
+                    $asar = "Asar : ".$dataSholatJSON['items']['0']['asr'];
+                    $magrib = "Maghrib : ".$dataSholatJSON['items']['0']['maghrib'];
+                    $isya = "Isya : ".$dataSholatJSON['items']['0']['isha'];
+
+                    $textGabunganSholat = "Jadwal Sholat \nBojongsoang Bandung \n".$subuh."\n".$zuhur."\n".$asar."\n".$magrib."\n".$isya."\n";
+
+                    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($textGabunganSholat);
+                    $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+                    return $result->getHTTPStatus() . ' ' . $result->getRawBody();
                     break;
                 default:
                     break;
@@ -334,7 +447,11 @@ $app->get('/multicast', function($req, $res) use ($bot)
  
     // send multicast message to user
     $textMessageBuilder = new TextMessageBuilder('Halo, ini pesan multicast');
-    $result = $bot->multicast($userList, $textMessageBuilder);
+    for ($i=0; $i < 5 ; $i++) { 
+        # code...
+        $result = $bot->multicast($userList, $textMessageBuilder);
+    }
+    
    
     return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
 });
